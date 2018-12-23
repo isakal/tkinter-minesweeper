4be5b43ed821656d1d351d03e992c1067da6e14f
isakal <45316286+isakal@users.noreply.github.com>
2018-12-23 16:35:52 +0100

    Add files via upload
    Added dropdown menu

--- root.py
+++ root.py
@@ -12,16 +12,30 @@ class GUI:
         self.printButton = Button(frame1,text ="Print Message",command = self.printMessage)
         self.printButton.pack()
 
+        self.menu = Menu(root)
+        root.config(menu=self.menu)
+        self.submenu1 = Menu(self.menu)
+        self.menu.add_cascade(label="cool kids",menu=self.submenu1)
+        self.submenu1.add_command(label="esketit",command=self.doNothing)
+        self.submenu1.add_command(label="boi",command=self.boi)
+        self.submenu1.add_separator()
+        self.submenu1.add_command(label="Quit",command =quit)
+
+
         self.quitButton = Button(frame1,text="Quit",command=frame1.quit)
         self.quitButton.pack()
 
     def printMessage(self):
         self.messageBox = messagebox.showinfo("","Hello!")
+    def doNothing(self):
+        self.messageBox2 = messagebox.showinfo("","esketit")
+    def boi(self):
+        self.messageBox3 = messagebox.showinfo("","BOI")
 
 
 
 
 root = Tk()
-root.geometry("100x80")
+root.geometry("300x200")
 esketit = GUI(root)
 root.mainloop()

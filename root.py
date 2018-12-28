954fcd3823e85999b5d9b89c4182ac300dfccfa0
Ivan Sakal <45316286+isakal@users.noreply.github.com>
2018-12-28 11:18:06 +0100

    Fixed resizing the main frame with the difficulty
    Frame is now resizible and dimensions are changing when difficulty is changed

--- root.py
+++ root.py
@@ -5,76 +5,78 @@ from buttons import *
 
 
 class GUI:
-    def __init__(self, master):
-        self.master=master
-        global frame1
-        frame1 = Frame(master,background="grey17",width=300,height=300)
-        frame1.pack()
-        master.title("Minesweeper")
-        master.iconbitmap(r'mnswpr.ico')
-        master.geometry("300x300")
-        master.resizable(FALSE,FALSE)
-        #-----
-        frame1.configure(background="red")
+	def __init__(self, master):
+		self.master = master
+		global frame1
+		self.frame1 = Frame(master, background="grey17")
+		self.frame1.pack(fill='both', expand=True)
+		master.title("Minesweeper")
+		master.iconbitmap(r'mnswpr.ico')
 
 
-        self.menu = Menu(master, tearoff=0)
-        self.variable = IntVar()
+		master.resizable(FALSE, FALSE)
 
-        master.config(menu=self.menu)
-        self.submenu1 = Menu(self.menu,tearoff=0)
-        self.submenu2 = Menu(self.menu, tearoff=0)
-        self.submenu3 = Menu(self.menu, tearoff=0)
+		self.menu = Menu(master, tearoff=0)
+		self.variable = IntVar()
 
-        self.menu.add_cascade(label="Game", menu=self.submenu1)
-        self.submenu1.add_command(label="New Game")
-        self.submenu1.add_separator()
-        self.submenu1.add_radiobutton(label="Beginner",value=1, variable=self.variable, command=self.difficulty1)
-        self.submenu1.add_radiobutton(label="Intermediate",value=2, variable=self.variable, command=self.difficulty2)
-        self.submenu1.add_radiobutton(label="Expert",value=3, variable=self.variable, command=self.difficulty3)
-        self.variable.set(1)
-        self.difficulty1()
+		master.config(menu=self.menu)
+		self.submenu1 = Menu(self.menu, tearoff=0)
+		self.submenu2 = Menu(self.menu, tearoff=0)
+		self.submenu3 = Menu(self.menu, tearoff=0)
 
-        self.submenu1.add_separator()
-        self.submenu1.add_command(label="Quit",command=self.QuitPrompt)
+		self.menu.add_cascade(label="Game", menu=self.submenu1)
+		self.submenu1.add_command(label="New Game")
+		self.submenu1.add_separator()
+		self.submenu1.add_radiobutton(label="Beginner", value=1, variable=self.variable, command=self.difficulty1)
+		self.submenu1.add_radiobutton(label="Intermediate", value=2, variable=self.variable, command=self.difficulty2)
+		self.submenu1.add_radiobutton(label="Expert", value=3, variable=self.variable, command=self.difficulty3)
+		self.variable.set(1)
+		self.difficulty1()
 
-        self.menu.add_cascade(label="Options", menu=self.submenu2)
-        self.submenu2.add_checkbutton(label="Tutorial coming soon")
+		self.submenu1.add_separator()
+		self.submenu1.add_command(label="Quit", command=self.QuitPrompt)
 
-        self.submenu3.add_command(label="Instructions",command=self.InstructionsInChrome)
-        self.submenu3.add_separator()
-        self.menu.add_cascade(label="Help", menu=self.submenu3)
-        self.submenu3.add_command(label="About", command=self.AboutInChrome)
+		self.menu.add_cascade(label="Options", menu=self.submenu2)
+		self.submenu2.add_checkbutton(label="Tutorial coming soon")
 
-        #------
-        b11.pack()
-        
+		self.submenu3.add_command(label="Instructions", command=self.InstructionsInChrome)
+		self.submenu3.add_separator()
+		self.menu.add_cascade(label="Help", menu=self.submenu3)
+		self.submenu3.add_command(label="About", command=self.AboutInChrome)
 
-    
-    def difficulty1(self):
-        self.master.geometry("300x300")
-    
-    def difficulty2(self):
-        self.master.geometry("600x300")
-    
-    def difficulty3(self):
-        self.master.geometry("1000x800")
 
-    def QuitPrompt(self):
-        self.quitPrompt = messagebox.askquestion("Quit", "Are You Sure you want to exit?", icon="warning")
-        if self.quitPrompt.lower() == "yes":
-            self.master.destroy()
-    def InstructionsInChrome(self):
-        webbrowser.open('www.freeminesweeper.org/help/minehelpinstructions.html')
-    def AboutInChrome(self):
-        webbrowser.open('http://www.freeminesweeper.org/help/mineabout.html')
 
-    if __name__ == '__main__':
-        print("started game")
-        mainloop()
-        print("finished game")
+
+
+
+	def difficulty1(self):
+		self.master.geometry("300x300")
+		self.frame1 = Frame(self.master, background="grey17", width=300, height=300)
+
+	def difficulty2(self):
+		self.master.geometry("600x300")
+		self.frame1 = Frame(self.master, background="grey17", width=600, height=300)
+
+	def difficulty3(self):
+		self.master.geometry("1000x800")
+		self.frame1 = Frame(self.master, background="grey17", width=1000, height=800)
+
+	def QuitPrompt(self):
+		self.quitPrompt = messagebox.askquestion("Quit", "Are You Sure you want to exit?", icon="warning")
+		if self.quitPrompt.lower() == "yes":
+			self.master.destroy()
+
+	def InstructionsInChrome(self):
+		webbrowser.open('www.freeminesweeper.org/help/minehelpinstructions.html')
+
+	def AboutInChrome(self):
+		webbrowser.open('http://www.freeminesweeper.org/help/mineabout.html')
+
+	if __name__ == '__main__':
+		print("started game")
+		mainloop()
+		print("finished game")
 
 
 root = Tk()
 minesweeper = GUI(root)
-

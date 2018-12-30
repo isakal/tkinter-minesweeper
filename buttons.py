c4c40df2e99fa29422bda506943cb0bef74cfd50
isakal <sakyYT@gmail.com>
2018-12-30 16:10:37 +0100

    Playing around and trying to collapse pressed button

--- buttons.py
+++ buttons.py
@@ -1,28 +1,41 @@
 from tkinter import *
-buttonSize=25
-buttonsDict={}
+
+buttonSize = 25
+buttonsDict = {}
 
 
 def buttonsDiff1(frame):
+	global gridButton
 	buttonsDict.clear()
-	for row in range(0,10):
-		for column in range(0,10):
-			gridButton = Button(frame)
-			buttonsDict["field{}{}".format(row,column)]=gridButton
-			buttonsDict["field{}{}".format(row,column)].place(height=buttonSize,width=buttonSize,x=row*buttonSize,y=column*buttonSize)
+	for row in range(0, 10):
+		for column in range(0, 10):
+			gridButton = Button(frame, relief=RAISED,command=CollapseButton)
+			buttonsDict["field {}{}".format(row, column)] = gridButton
+			buttonsDict["field {}{}".format(row, column)].place(height=buttonSize, width=buttonSize, x=row * buttonSize,
+								 							   y=column * buttonSize)
+
+
+
 
 def buttonsDiff2(frame):
+	global gridButton
 	buttonsDict.clear()
-	for row in range(0,15):
-		for column in range(0,10):
-			gridButton = Button(frame)
-			buttonsDict["field{}{}".format(row,column)]=gridButton
-			buttonsDict["field{}{}".format(row,column)].place(height=buttonSize,width=buttonSize,x=row*buttonSize,y=column*buttonSize)
+	for row in range(0, 15):
+		for column in range(0, 10):
+			gridButton = Button(frame, relief=RAISED,command=CollapseButton)
+			buttonsDict["field {}{}".format(row, column)] = gridButton
+			buttonsDict["field {}{}".format(row, column)].place(height=buttonSize, width=buttonSize, x=row * buttonSize,
+															   y=column * buttonSize)
+
 
 def buttonsDiff3(frame):
+	global gridButton
 	buttonsDict.clear()
-	for row in range(0,20):
-		for column in range(0,15):
-			gridButton = Button(frame)
-			buttonsDict["field{}{}".format(row,column)]=gridButton
-			buttonsDict["field{}{}".format(row,column)].place(height=buttonSize,width=buttonSize,x=row*buttonSize,y=column*buttonSize)
+	for row in range(0, 20):
+		for column in range(0, 15):
+			gridButton = Button(frame, relief=RAISED,command=CollapseButton)
+			buttonsDict["field {}{}".format(row, column)] = gridButton
+			buttonsDict["field {}{}".format(row, column)].place(height=buttonSize, width=buttonSize, x=row * buttonSize,
+															   y=column * buttonSize)
+def CollapseButton():
+	gridButton.config(relief=SUNKEN)

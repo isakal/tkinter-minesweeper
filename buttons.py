10ab9f6705d45f9a1c1490b1e6af8ad48ceb5257
Spinzed <davor.najev@gmail.com>
2018-12-31 10:46:42 +0100

    Removed unnecessary functions from buttons.py

--- buttons.py
+++ buttons.py
@@ -18,25 +18,5 @@ def buttonsDiff1(frame, rows, columns):
 											 y=row * buttonSize)
 
 
-def buttonsDiff2(frame):
-	buttonsDict.clear()
-	for row in range(0, 15):
-		for column in range(0, 10):
-			gridButton = Button(frame, bg="grey75", command=lambda row=row, column=column: sink(row, column))
-			buttonsDict[(row, column)] = gridButton
-			buttonsDict[(row, column)].place(height=buttonSize, width=buttonSize, x=row * buttonSize,
-											 y=column * buttonSize)
-
-
-def buttonsDiff3(frame):
-	buttonsDict.clear()
-	for row in range(0, 20):
-		for column in range(0, 15):
-			gridButton = Button(frame, bg="grey75", command=lambda row=row, column=column: sink(row, column))
-			buttonsDict[(row, column)] = gridButton
-			buttonsDict[(row, column)].place(height=buttonSize, width=buttonSize, x=row * buttonSize,
-											 y=column * buttonSize)
-
-
 def sink(row, column):
 	buttonsDict[(row, column)].config(relief=SUNKEN, bg="white",state=DISABLED)
